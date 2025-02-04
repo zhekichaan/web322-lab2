742f4836e6e92cb03906779004a2130070376e00
zhekichaan <y.chernytskyi@gmail.com>
2025-02-03 20:00:47 -0500

    fix

--- server.js
+++ server.js
@@ -10,34 +10,28 @@ app.get("/", (req, res) =>
   res.send("Assignment 2: Yevhen Chernytskyi - 166613232")
 );
 app.get("/solutions/projects", (req, res) =>
-  res.send(
-    projectData.getAllProjects().then((data) => {
-      return data;
-    })
-  )
+  projectData.getAllProjects().then((data) => {
+    res.send(data);
+  })
 );
 app.get("/solutions/projects/id-demo", (req, res) =>
-  res.send(
-    projectData
-      .getProjectById(9)
-      .then((data) => {
-        return data;
-      })
-      .catch((reason) => {
-        return reason;
-      })
-  )
+  projectData
+    .getProjectById(9)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((reason) => {
+      res.send(reason);
+    })
 );
 app.get("/solutions/projects/sector-demo", (req, res) =>
-  res.send(
-    projectData
-      .getProjectsBySector("transp")
-      .then((data) => {
-        return data;
-      })
-      .catch((reason) => {
-        return reason;
-      })
-  )
+  projectData
+    .getProjectsBySector("transp")
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((reason) => {
+      res.send(reason);
+    })
 );
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
