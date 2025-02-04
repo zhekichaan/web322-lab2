2cc743d710e920e89ecf39be5c4907d5a12ef644
zhekichaan <y.chernytskyi@gmail.com>
2025-02-03 20:55:42 -0500

    added comments

--- modules/projects.js
+++ modules/projects.js
@@ -1,28 +1,30 @@
 const projectData = require("../data/projectData");
 const sectorData = require("../data/sectorData");
+// importing data files
 
-let projects = [];
+let projects = []; // creating empty array for data
 
 const initialize = () => {
   return new Promise((resolve, reject) => {
     projectData.map((project) => {
+      // map through each proeject in projectData
       project.sector = sectorData.find(
-        (element) => element.id == project.sector_id
+        (element) => element.id == project.sector_id // look for a sector with the same id as sector_id in project
       ).sector_name;
-      projects.push(project);
+      projects.push(project); // push to the array
     });
-    resolve();
+    resolve(); // resolve with no data
   });
 };
 
 const getAllProjects = () => {
   return new Promise((resolve, reject) => {
-    resolve(projects);
+    resolve(projects); // resolve with all projects
   });
 };
 
 const getProjectById = (projectID) => {
-  const result = projects.find((project) => project.id == projectID);
+  const result = projects.find((project) => project.id == projectID); // look for one project with projectID
 
   return new Promise((resolve, reject) => {
     if (result) resolve(result);
@@ -31,16 +33,18 @@ const getProjectById = (projectID) => {
 };
 
 const getProjectsBySector = (sector) => {
-  const result = projects.filter((project) =>
-    project.sector.toLowerCase().includes(sector.toLowerCase())
+  const result = projects.filter(
+    (project) => project.sector.toLowerCase().includes(sector.toLowerCase()) // look for projects that include sector
   );
 
   return new Promise((resolve, reject) => {
-    if (result.length > 0) resolve(result);
+    if (result.length > 0)
+      resolve(result); // if 1 or more found resolve with result
     else reject(`Unable to find a project with sector: ${sector}.`);
   });
 };
 
+// export functions
 module.exports = {
   initialize,
   getAllProjects,
--- server.js
+++ server.js
@@ -10,32 +10,33 @@
  *
  ********************************************************************************/
 
-const projectData = require("./modules/projects");
+const projectData = require("./modules/projects"); // import functions
 
-projectData.initialize();
+projectData.initialize(); // initialize object
 
-const express = require("express");
-const app = express();
-const port = 3000;
+const express = require("express"); // import express package
+const app = express(); // create express application
+const port = 3000; // set port to 3000
 
-app.get("/", (req, res) =>
-  res.send("Assignment 2: Yevhen Chernytskyi - 166613232")
+app.get(
+  "/",
+  (req, res) => res.send("Assignment 2: Yevhen Chernytskyi - 166613232") // send message on "/" route
 );
 
 app.get("/solutions/projects", (req, res) =>
   projectData.getAllProjects().then((data) => {
-    res.send(data);
+    res.send(data); // send data of all projects
   })
 );
 
 app.get("/solutions/projects/id-demo", (req, res) => {
-  const result = projectData
+  projectData
     .getProjectById(3)
     .then((data) => {
-      res.send(data);
+      res.send(data); // if projects with this id exists then data sends to the user
     })
     .catch((reason) => {
-      res.send(reason);
+      res.send(reason); // if there is no project with this id, error message sends to the user
     });
 });
 
@@ -43,11 +44,12 @@ app.get("/solutions/projects/sector-demo", (req, res) =>
   projectData
     .getProjectsBySector("transp")
     .then((data) => {
-      res.send(data);
+      res.send(data); // if at least one project includes this sector (or its part) then data sends
     })
     .catch((reason) => {
-      res.send(reason);
+      res.send(reason); // if there is no project with this sector, error message returns
     })
 );
 
+// listen for connections
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
